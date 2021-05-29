6dd0da25c6ea81813337886fe6c9ea1e30ffd75a
ORiveraJr84 <oriverajr84@gmail.com>
2021-05-28 23:17:57 -0400

    Created down schema

--- data/migrations/20210529025148_01-initial.js
+++ data/migrations/20210529025148_01-initial.js
@@ -46,4 +46,10 @@ exports.up = function (knex) {
     });
 };
 
-exports.down = function (knex) {};
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("project_resources")
+    .dropTableIfExists("task")
+    .dropTableIfExists("resource")
+    .dropTableIfExists("projects");
+};
