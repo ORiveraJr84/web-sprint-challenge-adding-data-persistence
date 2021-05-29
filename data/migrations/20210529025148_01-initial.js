e8f88f28726f2563bb830ea30841bccbb51e5c68
ORiveraJr84 <oriverajr84@gmail.com>
2021-05-28 23:59:03 -0400

    Ran first migration

--- data/migrations/20210529025148_01-initial.js
+++ data/migrations/20210529025148_01-initial.js
@@ -2,18 +2,18 @@ exports.up = function (knex) {
   return knex.schema
     .createTable("projects", (tbl) => {
       tbl.increments("project_id");
-      tbl.text("project_name").required();
+      tbl.text("project_name").notNullable();
       tbl.text("project_description");
       tbl.boolean("project_completed").defaultTo(0);
     })
     .createTable("resource", (tbl) => {
       tbl.increments("resource_id");
-      tbl.text("resource_name").required().unique();
+      tbl.text("resource_name").notNullable().unique();
       tbl.text("resource_description");
     })
     .createTable("task", (tbl) => {
       tbl.increments("task_id");
-      tbl.text("task_description").required();
+      tbl.text("task_description").notNullable();
       tbl.text("task_notes");
       tbl.boolean("task_completed").defaultTo(0);
       tbl
@@ -23,7 +23,7 @@ exports.up = function (knex) {
         .references("project_id")
         .inTable("projects")
         .onUpdate("CASCADE")
-        .onDelete("CASCADES");
+        .onDelete("CASCADE");
     })
     .createTable("project_resources", (tbl) => {
       tbl.increments("connector_id");
@@ -34,7 +34,7 @@ exports.up = function (knex) {
         .references("project_id")
         .inTable("projects")
         .onUpdate("CASCADE")
-        .onDelete("CASCADES");
+        .onDelete("CASCADE");
       tbl
         .integer("resource_id")
         .unsigned()
@@ -42,7 +42,7 @@ exports.up = function (knex) {
         .references("resource_id")
         .inTable("resources")
         .onUpdate("CASCADE")
-        .onDelete("CASCADES");
+        .onDelete("CASCADE");
     });
 };
 
