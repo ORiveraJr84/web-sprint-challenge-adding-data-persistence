a75f910bda58e676e367718a070029f29bd43f50
ORiveraJr84 <oriverajr84@gmail.com>
2021-05-28 23:00:20 -0400

    Created schema for projects table

--- data/migrations/20210529025148_01-initial.js
+++ data/migrations/20210529025148_01-initial.js
@@ -1,6 +1,12 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+  .createTable("projects", tbl => {
+      tbl.increments("project_id")
+      tbl.text("project_name").required()
+      tbl.text("project_description")
+      tbl.boolean("project_completed").defaultTo(0)
+  })
 };
 
 exports.down = function(knex) {
