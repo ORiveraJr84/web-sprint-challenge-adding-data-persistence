94df67d8fdda5fd8b760d2cc4d9f75724064c198
ORiveraJr84 <oriverajr84@gmail.com>
2021-05-28 23:02:33 -0400

    Created schema for resource table

--- data/migrations/20210529025148_01-initial.js
+++ data/migrations/20210529025148_01-initial.js
@@ -7,6 +7,11 @@ exports.up = function(knex) {
       tbl.text("project_description")
       tbl.boolean("project_completed").defaultTo(0)
   })
+  .createTable("resource", tbl => {
+      tbl.increments("resource_id")
+      tbl.text("resource_name").required().unique()
+      tbl.text("resource_description")
+  })
 };
 
 exports.down = function(knex) {
